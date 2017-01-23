39171e244c7190ba911e16f8a9b90a32f8a456b4
Josephine Elder <parusmontanus@gmail.com>
2017-01-23 19:04:03 +0000

    Testing saving to and retrieving from database fails

--- specs/fx_rate_spec.rb
+++ specs/fx_rate_spec.rb
@@ -17,12 +17,18 @@ class TestFxRate < MiniTest::Test
     assert_equal("DKK", @fx_rate.currency)
   end
 
-  def test_can_save_to_database
-    puts @fx_rate.rate
-    @fx_rate.save
-    @test_rate = FxRate.fx_rate(1)
-    assert_equal(6.8922, @test_rate.rate)
+  def test_can_save_to_db_and_retrieve_single_record
+    test_rate = FxRate.new({
+      'rate_date' => "2017-01-23",
+      'currency' => "999",
+      'rate' => 9.999
+    })
+    test_rate.save
+    retrieve_rate = FxRate.fx_rate(test_rate.id)
+    assert_equal(9.999, retrieve_rate.rate)
   end
+
+
 
 
 
