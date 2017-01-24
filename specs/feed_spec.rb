f4e77465f7885fb0f1e92e81267c06d8e59e637b
Josephine Elder <parusmontanus@gmail.com>
2017-01-24 09:38:26 +0000

    Fixed bug with date

--- specs/feed_spec.rb
+++ specs/feed_spec.rb
@@ -5,29 +5,32 @@ require 'Nokogiri'
 require_relative '../models/feed.rb'
 # require_relative '../models/fx_rate.rb'
 
-class TestFxRate < MiniTest::Test
+class TestFeed < MiniTest::Test
 
   def setup
-    source = "http://fakesource.lum"
+    fake_source = "http://fakesource.lum"
+    @fake_feed = Feed.new(fake_source)
+    source = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml"
     @feed = Feed.new(source)
+    @date_today = Time.now.to_s[0..9]
   end
 
   def test_feed_has_source
-    assert_equal("http://fakesource.lum", @feed.source)
+    assert_equal("http://fakesource.lum", @fake_feed.source)
   end
 
   def test_feed_fetches_rates_xml
-    date_today = Time.now.to_s[0..9]
-    feed = Feed.new("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml")
-    doc = feed.get_data
-    feed_date_today = doc.xpath('//xmlns:Cube')[0].elements[0].attribute('time').value
-    assert_equal(date_today, feed_date_today)
+    doc = @feed.get_data
+    first_currency = doc.xpath('//xmlns:Cube')[0].elements[0].elements[0].attributes["currency"].value
+    assert_equal("USD", first_currency)
+  end
+
+  def test_feed_parses_xml
+    fx_rates_array = @feed.parse_xml
+    assert(fx_rates_array.is_a?(Array))
   end
 
 
+
+
 end
-
-
-
-
-
