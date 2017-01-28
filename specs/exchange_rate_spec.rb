c12eb6a6b8ad24b0d1d8a33dce91bf99d671d946
Josephine Elder <parusmontanus@gmail.com>
2017-01-28 16:29:31 +0000

    Writing currency conversion test

--- specs/exchange_rate_spec.rb
+++ specs/exchange_rate_spec.rb
@@ -12,7 +12,16 @@ class TestExchangeRate < MiniTest::Test
     assert_equal(0.86055, rate_against_euro)
   end
 
-  
+  def test_exchange_rate_can_calculate_conversion
+    options = {
+      "hkd_amount" => 100.00,
+      "from_currency" => "HKD",
+      "to_currency" => "USD",
+      "date_requested" => "2016-12-28"
+    }
+    converted_amount = ExchangeRate.convert(options)
+    assert_equal(12.8894, converted_amount)
+  end
 
 
 end
