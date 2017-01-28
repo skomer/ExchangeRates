24eb990c70d999b8a759c131dd9025f4fec151b1
Josephine Elder <parusmontanus@gmail.com>
2017-01-28 16:55:36 +0000

    Reorganising ExchangeRate methods

--- models/exchange_rate.rb
+++ models/exchange_rate.rb
@@ -4,12 +4,41 @@ require_relative 'rate_record.rb'
 
 class ExchangeRate
   
-  def self.at(date_requested, from_currency, to_currency)
-    rate_record = RateRecord.rate_record_by_date_and_currency(date_requested, from_currency)
-    return rate_record.rate
+  def self.get_rate(date_requested, from_currency, to_currency)
+    from_currency_rate_record = RateRecord.rate_record_by_date_and_currency(date_requested, from_currency)
+    to_currency_rate_record = RateRecord.rate_record_by_date_and_currency(date_requested, to_currency)
+    return [from_currency_rate_record.rate, to_currency_rate_record.rate]
   end
 
+  def self.calculate_from_to_currency_rate(from_currency_rate, to_currency_rate)
 
+
+  end
+
+  # def self.convert(options)
+  #   from_currency_rate_against_euro = at(
+  #     options.fetch("date_requested"),
+  #     options.fetch("from_currency"),
+  #     options.fetch("to_currency")
+  #   )
+
+  #   to_currency_rate_against_euro = at(
+  #     options.fetch("date_requested"),
+  #     options.fetch("from_currency"),
+  #     options.fetch("to_currency")
+  #   )
+
+
+
+  #   converted_amount = (
+  #     (options.fetch("amount_to_convert") / )
+
+
+
+  #   )
+
+
+  # end
 
 
 
--- specs/exchange_rate_spec.rb
+++ specs/exchange_rate_spec.rb
@@ -8,20 +8,32 @@ require_relative '../models/exchange_rate.rb'
 class TestExchangeRate < MiniTest::Test
 
   def test_exchange_rate_can_fetch_rate_from_db
-    rate_against_euro = ExchangeRate.at("2016-11-17", "GBP", "MXN")
-    assert_equal(0.86055, rate_against_euro)
+    gbp_rate_against_euro = ExchangeRate.get_rate("2016-11-17", "GBP", "MXN")[0]
+    mxn_rate_against_euro = ExchangeRate.get_rate("2016-11-17", "GBP", "MXN")[1]
+    assert_equal(0.86055, gbp_rate_against_euro)
+    assert_equal(21.7504, mxn_rate_against_euro)
+
   end
 
-  def test_exchange_rate_can_calculate_conversion
-    options = {
-      "hkd_amount" => 100.00,
-      "from_currency" => "HKD",
-      "to_currency" => "USD",
-      "date_requested" => "2016-12-28"
-    }
-    converted_amount = ExchangeRate.convert(options)
-    assert_equal(12.8894, converted_amount)
+  def test_exchange_rate_can_calculate_from_to_currency_rate
+    gbp_rate_against_euro = ExchangeRate.get_rate("2016-11-17", "GBP", "MXN")[0]
+    mxn_rate_against_euro = ExchangeRate.get_rate("2016-11-17", "GBP", "MXN")[1]
+    gbp_to_mxn_rate = ExchangeRate.calculate_from_to_currency_rate(gbp_rate_against_euro, mxn_rate_against_euro)
+    assert_equal(25.2750, gbp_to_mxn_rate)
   end
+
+
+
+  # def test_exchange_rate_can_calculate_conversion
+  #   options = {
+  #     "amount_to_convert" => 100.00,
+  #     "from_currency" => "HKD",
+  #     "to_currency" => "USD",
+  #     "date_requested" => "2016-12-28"
+  #   }
+  #   converted_amount = ExchangeRate.convert(options)
+  #   assert_equal(12.8894, converted_amount)
+  # end
 
 
 end
