de0479bbcfd5049c5fc938a38122afcecaa728bc
Josephine Elder <parusmontanus@gmail.com>
2017-01-28 16:16:17 +0000

    Restructuring ExchangeRate class

--- models/exchange_rate.rb
+++ models/exchange_rate.rb
@@ -3,17 +3,8 @@ require_relative 'rate_record.rb'
 
 
 class ExchangeRate
-
-  attr_reader :date_requested, :from_currency, :to_currency
-
-  def initialize(date_requested, from_currency, to_currency)
-    @date_requested = date_requested
-    @from_currency = from_currency
-    @to_currency = to_currency
-  end
-
   
-  def at(date_requested, from_currency, to_currency)
+  def self.at(date_requested, from_currency, to_currency)
     rate_record = RateRecord.rate_record_by_date_and_currency(date_requested, from_currency)
     return rate_record.rate
   end
--- specs/exchange_rate_spec.rb
+++ specs/exchange_rate_spec.rb
@@ -7,22 +7,12 @@ require_relative '../models/exchange_rate.rb'
 
 class TestExchangeRate < MiniTest::Test
 
-  def setup
-    date_requested = "2016-11-17"
-    from_currency = "GBP"
-    to_currency = "MXN"
-    @exchange_rate = ExchangeRate.new(date_requested, from_currency, to_currency)
-  end
-
-  def test_exchange_rate_has_date_requested
-    assert_equal("2016-11-17", @exchange_rate.date_requested)
-  end
-
   def test_exchange_rate_can_fetch_rate_from_db
-    rate_against_euro = @exchange_rate.at(@exchange_rate.date_requested, @exchange_rate.from_currency, @exchange_rate.to_currency);
+    rate_against_euro = ExchangeRate.at("2016-11-17", "GBP", "MXN")
     assert_equal(0.86055, rate_against_euro)
   end
 
+  
 
 
 end
