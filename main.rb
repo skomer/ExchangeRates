c95e2a28848dab5e2b929b0dabcd79bce0beea17
Josephine Elder <parusmontanus@gmail.com>
2017-01-28 20:17:14 +0000

    Testing ExchangeRate returns from/to currency rate given arguments passes

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/contrib/all'
 require 'pry-byebug'
 require_relative 'rate_record_controller.rb'
+require_relative 'feed_controller.rb'
 
 get '/' do
   erb :home
--- models/exchange_rate.rb
+++ models/exchange_rate.rb
@@ -1,12 +1,15 @@
 require 'date'
+require 'pry-byebug'
 require_relative 'rate_record.rb'
 
 
 class ExchangeRate
   
   def self.get_rate(date_requested, from_currency, to_currency)
+    binding.pry
     from_currency_rate_record = RateRecord.rate_record_by_date_and_currency(date_requested, from_currency)
     to_currency_rate_record = RateRecord.rate_record_by_date_and_currency(date_requested, to_currency)
+    binding.pry
     return [from_currency_rate_record.rate, to_currency_rate_record.rate]
   end
 
@@ -15,8 +18,17 @@ class ExchangeRate
     return from_to_currency_rate.round(5)
   end
 
+  def self.at(date_requested, from_currency, to_currency)
+    from_currency_rate_against_euro = get_rate(date_requested, from_currency, to_currency)[0]
+    to_currency_rate_against_euro = get_rate(date_requested, from_currency, to_currency)[1]
+    from_to_currency_rate = calculate_from_to_currency_rate(from_currency_rate_against_euro, to_currency_rate_against_euro)
+    return from_to_currency_rate
+  end
 
   # ExchangeRate.at(Date.today,'GBP','USD')
   # GBP is from currency and USD to currency
 
 end
+
+
+
